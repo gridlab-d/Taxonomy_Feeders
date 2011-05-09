2cddc5f98a11e8ffd90ccc31e202ff20ab9c3347
Frank Tuffner <Francis.Tufner@pnnl.gov>
2011-05-09 22:10:46 +0000

    Change to figure size function so the figure size is fixed based on pixel, not monitor resolution.

--- PopulationScript/PostProcessingScripts/set_figure_size.m
+++ PopulationScript/PostProcessingScripts/set_figure_size.m
@@ -1,6 +1,9 @@
 function [] = set_figure_size(my_name)
 
-    scrsz = get(0,'ScreenSize');
-    figure('Name',my_name,'NumberTitle','off','Position',[100 scrsz(4)/15 scrsz(3)/1.2 scrsz(4)/2]);
+    %Original code - set size based on 20" wide screen monitor at 1680x1050
+    %Locked to a static size so different monitor sizes/resolutions don't cause issues
+    %%% scrsz = get(0,'ScreenSize');
+    %%% figure('Name',my_name,'NumberTitle','off','Position',[100 scrsz(4)/15 scrsz(3)/1.2 scrsz(4)/2]);
+    figure('Name',my_name,'NumberTitle','off','Position',[100 70 1400 525]);
     
 end
