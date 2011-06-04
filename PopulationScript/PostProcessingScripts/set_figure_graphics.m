238738731692211079469e4c5360764f0f7277c8
Jason Fuller <Jason.Fuller@pnnl.gov>
2011-06-04 00:40:38 +0000

    Added a little more documentation and another option for y-axis

--- PopulationScript/PostProcessingScripts/set_figure_graphics.m
+++ PopulationScript/PostProcessingScripts/set_figure_graphics.m
@@ -1,16 +1,26 @@
 function [] = set_figure_graphics(xlabels,my_name,yformat,my_leg,offset,leg_loc)
-    % This file formats the graphics to match in all files
+    % This file formats the graphics to match in all files for the SGIG
+    % analysis
+    %
+    % set_figure_graphics(xlabels,my_name,yformat,my_leg,offset,leg_loc)
+    %
     % xlabels - an array of labels for the x-axis
     % my_name - name of the graphics file
-    % yformat - 1-removes exponens, 2 set 2 decimal places
+    % yformat - 1-removes exponens, 2 set 2 decimal places, 3 sets 1
+    %           decimal place
     % my_leg - lets you add a legend; set to 'none' to provide no legend
     % offset - moves the yaxis label to the left when the numbering on the
     %          yaxis is too large.
     % leg_loc - Location of the legend.
+    %
+    % started 5/1/2011 by J. Fuller
+    
     ca = gca;
     cf = gcf;
+    
     % turns on the y-grid lines
     set(ca,'XGrid','off','YGrid','on','ZGrid','off');
+    
     % this manually moves the y-axis labels over a little so when saving to
     % a jpg, the text doesn't overlap the axis
     ylab = get(ca,'Ylabel');
@@ -22,7 +32,6 @@ function [] = set_figure_graphics(xlabels,my_name,yformat,my_leg,offset,leg_loc)
     end
     ylab_pos = [ylab_pos_left ylab_pos_orig(2) ylab_pos_orig(3)];
     set(ylab,'position',ylab_pos-offset); % Move the label when the format changes
-
     
     % This puts a border around the figure
     %  Note, this doesn't support sub-titles well
@@ -36,11 +45,12 @@ function [] = set_figure_graphics(xlabels,my_name,yformat,my_leg,offset,leg_loc)
         set(gca,'YTickLabel',num2str(get(gca,'YTick').','%2.0f'));
     elseif (yformat == 2)
         set(gca,'YTickLabel',num2str(get(gca,'YTick').','%2.2f'));
-
+    elseif (yformat == 3)
+        set(gca,'YTickLabel',num2str(get(gca,'YTick').','%0.1f'));
     end
     
     if (~strcmp(char(my_leg(1,:)),'none'))
-        legend(ca,my_leg,'location',leg_loc);
+        legend(ca,my_leg,'location',leg_loc,'FontSize',10);
     end
     
     % Resize the print-out image to only take up half a page minus a little
